48606fb6f1ffa7dbbaf615ba6b70305645de6567
inciner8r <punarvpawade17@gmail.com>
2022-03-23 00:56:05 +0530

    moar stuffs

--- main.go
+++ main.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type blog struct {
+	Title       string `json:"title"`
+	DateTime    string `json:"date"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
+}
+
 func main() {
 	r := mux.NewRouter()
 	http.ListenAndServe(":4000", r)
