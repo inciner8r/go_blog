477d6de1285f4e7859b00f3ae2068ac78cfc77d1
inciner8r <punarvpawade17@gmail.com>
2022-03-23 15:46:54 +0530

    init working mongo setup

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,7 +9,8 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/rs/cors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type blog struct {
@@ -18,18 +20,38 @@ type blog struct {
 	Content     string `json:"content"`
 }
 
-func setup() {
-
-}
-
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	k := os.Getenv("MONGOURI")
-	fmt.Println(k)
+	mongoURI := os.Getenv("MONGOURI")
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := client.Database("blog")
+	blogsCollection := db.Collection("blogs")
+
+	blog := blog{"a", "b", "c", "d"}
+	CreateBook(blog, blogsCollection, context.TODO())
+
 	r := mux.NewRouter()
-	handler := cors.Default().Handler(r)
-	http.ListenAndServe(":4000", handler)
+	// handler := cors.Default().Handler(r)
+	http.ListenAndServe(":4000", r)
+}
+
+func CreateBook(b blog, blogsCollection *mongo.Collection, ctx context.Context) (string, error) {
+	result, err := blogsCollection.InsertOne(ctx, b)
+	if err != nil {
+		return "0", err
+	}
+	return fmt.Sprintf("%v", result.InsertedID), err
 }
